1f52e5928e9e10b34830ddb0e4cefa72855af211
Nguyen Minh Khoi <nekomya1337@gmail.com>
2018-11-02 13:27:14 +0700

    Limited game Loop to 60 FPS (or specified by TARGET_FRAME)

--- src/Container/MyFrame.java
+++ src/Container/MyFrame.java
@@ -2,15 +2,19 @@ package Container;
 
 import Core.Game;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 //Creates and maintain the Window of the Game
 
 public class MyFrame extends JFrame {
 
+    private JPanel _containerPane;
     private Game _game;
 
     public MyFrame() {
+        _containerPane = new JPanel(new BorderLayout());
+
         GamePanel _gamePanel = new GamePanel(this);
 
         add(_gamePanel);
--- src/Core/Game.java
+++ src/Core/Game.java
@@ -11,9 +11,13 @@ import java.awt.image.BufferStrategy;
 public class Game extends Canvas {
     Frame _frame;
 
+    //GAME PARAMETERS
     private boolean _running = true;
+    private boolean _square = true;
 
     public static final String TITLE = "KaBoomMan";
+
+    private static final double TARGET_FRAME = 60.0;
 
     public static int WIDTH = 400;
     public static int HEIGHT = 400;
@@ -26,12 +30,32 @@ public class Game extends Canvas {
     public void start() {
         _running = true;
 
+        long lastTime = System.nanoTime();
+        long timer = System.currentTimeMillis();
+        final double ns = 1000000000.0 / TARGET_FRAME; //60 Frames per second
+
+        double delta = 0;
+
+        requestFocus();
+
         while(_running) {
-            renderScreen();
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+
+            if (delta >= 1) {
+                _square = !_square;
+                renderScreen(_square);
+                delta--;
+            }
         }
     }
 
-    private void renderScreen() {
+    private void runLoop() {
+
+    }
+
+    private void renderScreen(boolean _square) {
         BufferStrategy _strategy = this.getBufferStrategy();
 
         if (_strategy == null) {
@@ -41,12 +65,19 @@ public class Game extends Canvas {
 
         Graphics g = _strategy.getDrawGraphics();
 
-        g.setColor(Color.BLACK);
-        g.fillRect(0,0,WIDTH,HEIGHT);
-        g.setColor(Color.GREEN);
-        g.fillRect(0,0,WIDTH,HEIGHT);
+        if (_square) {
+            g.setColor(Color.BLUE);
+            g.fillRect(0,0,WIDTH,HEIGHT);
+        } else {
+            g.setColor(Color.GREEN);
+            g.fillRect(0,0,WIDTH,HEIGHT);
+        }
 
         g.dispose();
         _strategy.show();
+    }
+
+    private void updateGame() {
+
     }
 }
